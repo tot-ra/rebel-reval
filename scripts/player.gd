468e4d48e992c72513534033b7073316654a4d6d
Artjom Kurapov <artkurapov@gmail.com>
2025-08-06 01:43:23 +0300

    basic walking!

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
-@export var speed = 200
+@export var speed = 80
+
+@onready var animation_player = $AnimatedSprite2D
 
 func _physics_process(_delta):
 	var direction_x = Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up", "ui_down")
 	
-	print("Direction: ", Vector2(direction_x, direction_y))
-	
+	# --- Velocity ---
 	if direction_x:
 		velocity.x = direction_x * speed
 	else:
@@ -18,5 +19,26 @@ func _physics_process(_delta):
 	else:
 		velocity.y = move_toward(velocity.y, 0, speed)
 
-	print("Velocity: ", velocity)
+	update_animation()
 	move_and_slide()
+
+func update_animation():
+	var new_animation
+	
+	if velocity.length() > 0:
+		# Player is moving
+		if velocity.x > 0:
+			new_animation = "walk_right"
+		# Add other directions here in the future
+		# elif velocity.x < 0:
+		#	 new_animation = "walk_left"
+		else:
+			# Default to idle if moving in an unhandled direction (e.g., vertically)
+			new_animation = "idle" 
+	else:
+		# Player is stopped
+		new_animation = "idle"
+		
+	# Only change the animation if the state has changed
+	if animation_player.animation != new_animation:
+		animation_player.play(new_animation)
