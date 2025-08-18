85413f58d2be8c60ec90c5e187e00a23bf71b357
Artjom Kurapov <artkurapov@gmail.com>
2025-08-18 21:02:06 +0300

    ...

--- scenes/elements/door.gd
+++ scenes/elements/door.gd
@@ -7,8 +7,10 @@ class_name Door
 @export var spawn_direction = "up"
 
 @onready var spawn = $Spawn
-
+@onready var sound = $OpenSound
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
+		sound.play()
+		await sound.finished
 		DoorNavigator.go_to_scene(destination_level_tag, destination_door_tag)
