098119ae3a4eb0d11aa2a0a24d5cc876c2342fc5
Artjom Kurapov <artkurapov@gmail.com>
2025-08-19 03:04:30 +0300

    add running

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 class_name Player
 
-@export var speed = 160
+@export var speed = 100
+@export var run_speed = 200
 
 @onready var animation_player = $AnimatedSprite2D
 
@@ -15,19 +16,23 @@ func _on_spawn(position: Vector2, direction: String):
 	animation_player.stop()
 
 func _physics_process(_delta):
+	var current_speed = speed
+	if Input.is_action_pressed("ui_shift"):
+		current_speed = run_speed
+	
 	var direction_x = Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up", "ui_down")
 	
 	# --- Velocity ---
 	if direction_x:
-		velocity.x = direction_x * speed
+		velocity.x = direction_x * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
 		
 	if direction_y:
-		velocity.y = direction_y * speed
+		velocity.y = direction_y * current_speed
 	else:
-		velocity.y = move_toward(velocity.y, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, current_speed)
 
 	update_animation()
 	move_and_slide()
@@ -38,7 +43,10 @@ func update_animation():
 	new_animation = "idle" 
 	
 	if velocity.length() > 0:
-		new_animation = "walk"
+		if Input.is_action_pressed("ui_shift"):
+			new_animation = "run"
+		else:
+			new_animation = "walk"
 		
 		
 		if velocity.y > 0:
